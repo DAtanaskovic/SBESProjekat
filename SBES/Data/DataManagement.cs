acb45ab324b59f8ab04b2c12537c0d000e4cf5c0
DAtanskovic <dijana.atanaskovic997@gmail.com>
2019-12-10 11:36:37 +0100

    Dodavanje klase DataManagement

---
+++ SBES/Data/DataManagement.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Data
+{
+    public class DataManagement:IDataManagement
+    {
+        public string path { get; set; } = @"DataBase.txt";
+        List<EnergyConsumptionModel> listOfObjects = new List<EnergyConsumptionModel>();
+       public bool Create(EnergyConsumptionModel em)
+        {
+            bool isCreated = false;
+            if (!listOfObjects.Contains(em))
+            {
+                listOfObjects.Add(em);
+
+                string json = JsonConvert.SerializeObject(listOfObjects, Formatting.Indented);
+                File.WriteAllText(path, json);
+                isCreated = true;
+                Console.WriteLine("Creating is done successfully");
+                return isCreated;
+            }
+            else
+            {
+                Console.WriteLine("There is already the same object in file.");
+                isCreated = false;
+                return isCreated;
+            }
+
+        }
+
+        public bool Delete(int identificator)
+        {
+            bool isDeleted = false;
+            for (int i = 0; i < listOfObjects.Count; i++)
+            {
+                if (listOfObjects[i].identificator == identificator)
+                {
+                    listOfObjects.Remove(listOfObjects[i]);
+                    isDeleted = true;
+                    string json = JsonConvert.SerializeObject(listOfObjects, Formatting.Indented);
+                    File.WriteAllText(path, json);
+                    Console.WriteLine("Deleting is done successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Couldn't delete object with that identificator.");
+                    isDeleted = false;
+                }
+            }
+            return isDeleted;
+        }
+
+        public EnergyConsumptionModel Get(int identificator)
+        {
+            EnergyConsumptionModel em = new EnergyConsumptionModel();
+            for (int i = 0; i < listOfObjects.Count; i++)
+            {
+                if (listOfObjects[i].identificator == identificator)
+                {
+                    em = listOfObjects[i];
+                }
+            }
+            return em;
+
+        }
+
+        public List<EnergyConsumptionModel> GetAll()
+        {
+            return listOfObjects;
+        }
+
+        public bool Update(EnergyConsumptionModel em)
+        {
+            bool isUpdated = false;
+            for (int i = 0; i < listOfObjects.Count; i++)
+            {
+                if (listOfObjects[i].identificator == em.identificator)
+                {
+                    listOfObjects[i] = em;
+                    isUpdated = true;
+                    Console.WriteLine("Updating is done successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Couldn't update this object");
+                    isUpdated = false;
+                }
+            }
+            return isUpdated;
+        }
+
+    }
+    }
+
--- SBES/Data/EnergyConsumptionModel.cs
+++ SBES/Data/EnergyConsumptionModel.cs
@@ -8,53 +8,12 @@ namespace Data
 {
   public class EnergyConsumptionModel
     { 
-        public int identificator;
-        public string region;
-        public string city;
-        public int year;
-        public double usageOfElectricEnergyPerYear;
-        public string path;
-
-        public int Identificator
-        {
-            get { return identificator; }
-            set { identificator = value; }
-        }
-        
-
-        public string Region 
-        {
-            get { return region; }
-            set { region = value; }
-        }
+        public int identificator { get; set; }
+        public string region { get; set; }
+        public string city { get; set; }
+        public int year { get; set; }
+        public double usageOfElectricEnergyPerYear { get; set; }
       
-        public string City
-        {
-            get { return city; }
-            set { city = value; }
-        }
-
-   
-        public int Year 
-        {
-            get { return year; }
-            set { year = value; }
-        }
-
-
-        public double UsageOfElectricEnergyPerYear
-        {
-            get { return usageOfElectricEnergyPerYear; }
-            set { usageOfElectricEnergyPerYear= value; }
-        }
-       
-
-        public string Path
-        {
-            get { return path; }
-            set { path = value; }
-        }
-
 
 
         public EnergyConsumptionModel()
